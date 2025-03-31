29d3e7be201853650eb27f1b6c5e59d826fc98aa
BANDAHAELI <ali.khan.urf.bandali@gmail.com>
2025-03-31 08:03:48 +0500

    ali-xmd

---
+++ plugins/converters.js
@@ -0,0 +1,186 @@
+const {Sparky, isPublic,uploadMedia,handleMediaUpload} = require("../lib");
+const {getString, appendMp3Data} = require('./pluginsCore');
+const googleTTS = require('google-tts-api');
+const config = require('../config.js');
+const lang = getString('converters');
+
+
+Sparky({
+    name: "url",
+    fromMe: true,
+    desc: "",
+    category: "converters",
+  }, async ({ args, m }) => {
+    if (!m.quoted) {
+      return m.reply('Reply to an Image/Video/Audio');
+    }
+    try {
+        await m.react('⏫');
+      const mediaBuffer = await m.quoted.download();
+      const mediaUrl = await handleMediaUpload(mediaBuffer);
+      await m.react('✅');
+      m.reply(mediaUrl);
+    } catch (error) {
+        await m.react('❌');
+      m.reply('An error occurred while uploading the media.');
+    }
+  });
+
+Sparky({
+		name: "sticker",
+		fromMe: isPublic,
+		category: "converters",
+		desc: lang.STICKER_DESC
+	},
+	async ({
+		m,
+		args
+	}) => {
+		if (!m.quoted || !(m.quoted.message.imageMessage || m.quoted.message.videoMessage)) {
+			return await m.reply(lang.STICKER_ALERT);
+		}
+		await m.react('⏫');
+		await m.sendMsg(m.jid, await m.quoted.download(), {
+			packName: args.split(';')[0] || config.STICKER_DATA.split(';')[0],
+			authorName: args.split(';')[1] || config.STICKER_DATA.split(';')[1],
+			quoted: m
+		}, "sticker");
+		return await m.react('✅');
+	});
+
+
+Sparky({
+		name: "mp3",
+		fromMe: isPublic,
+		category: "converters",
+		desc: lang.MP3_DESC
+	},
+	async ({
+		m,
+		args
+	}) => {
+		if (!m.quoted || !(m.quoted.message.audioMessage || m.quoted.message.videoMessage || (m.quoted.message.documentMessage && m.quoted.message.documentMessage.mimetype === 'video/mp4'))) {
+			return await m.reply(lang.MP3_ALERT);
+		}
+		await m.react('⏫');
+		await m.sendMsg(m.jid, await appendMp3Data(await m.quoted.download(), args.split(";")[2] || config.AUDIO_DATA.split(";")[2], {
+                        title: args.split(";")[0] || config.AUDIO_DATA.split(";")[0],
+                        artist: args.split(";")[1] || config.AUDIO_DATA.split(";")[1]
+                }), {
+                        mimetype: 'audio/mpeg',
+			quoted: m
+		}, "audio");
+		return await m.react('✅');
+	});
+
+
+Sparky({
+		name: "take",
+		fromMe: isPublic,
+		category: "converters",
+		desc: lang.TAKE_DESC
+	},
+	async ({
+		m,
+		args
+	}) => {
+		if (!m.quoted || !m.quoted.message.stickerMessage) {
+			return await m.reply(lang.TAKE_ALERT);
+		}
+		await m.react('⏫');
+		await m.sendMsg(m.jid, await m.quoted.download(), {
+			packName: args.split(';')[0] || config.STICKER_DATA.split(';')[0],
+			authorName: args.split(';')[1] || config.STICKER_DATA.split(';')[1],
+			quoted: m
+		}, "sticker");
+		return await m.react('✅');
+	});
+
+
+Sparky({
+		name: "photo",
+		fromMe: isPublic,
+		category: "converters",
+		desc: lang.PHOTO_DESC
+	},
+	async ({
+		m
+	}) => {
+		if (!m.quoted || !m.quoted.message.stickerMessage || m.quoted.message.stickerMessage.isAnimated) {
+			return await m.reply(lang.PHOTO_ALERT);
+		}
+		await m.react('⏫');
+		await m.sendMsg(m.jid, await m.quoted.download(), {
+			quoted: m
+		}, "image");
+		return await m.react('✅');
+	});
+
+	Sparky(
+		{
+			name: "tts",
+			fromMe: isPublic,
+			category: "converters",
+			desc: "text to speech"
+		},
+		async ({
+			m, client, args
+		}) => {
+			if (!args) {
+				m.reply('_Enter Query!_')
+			} else {
+				let [txt,
+					lang] = args.split`:`
+				const audio = googleTTS.getAudioUrl(`${txt}`, {
+					lang: lang || "ml",
+					slow: false,
+					host: "https://translate.google.com",
+				})
+				client.sendMessage(m.jid, {
+					audio: {
+						url: audio,
+					},
+					mimetype: 'audio/mpeg',
+					ptt: true,
+					fileName: `${'tts'}.mp3`,
+				}, {
+					quoted: m,
+				})
+	
+			}
+		});
+
+
+Sparky(
+		{
+			name: "say",
+			fromMe: isPublic,
+			category: "converters",
+			desc: "text to speech"
+		},
+		async ({
+			m, client, args
+		}) => {
+			if (!args) {
+				m.reply('_Enter Query!_')
+			} else {
+				let [txt,
+					lang] = args.split`:`
+				const audio = googleTTS.getAudioUrl(`${txt}`, {
+					lang: lang || "en",
+					slow: false,
+					host: "https://translate.google.com",
+				})
+				client.sendMessage(m.jid, {
+					audio: {
+						url: audio,
+					},
+					mimetype: 'audio/mpeg',
+					ptt: true,
+					fileName: `${'tts'}.mp3`,
+				}, {
+					quoted: m,
+				})
+	
+			}
+		});
